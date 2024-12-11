77102ede919227346f2b557c040da777f40b5956
mostafa yehia <96825436+mostafayehia2002@users.noreply.github.com>
2024-12-11 19:08:04 +0200

    edit+2

--- resources/views/dashboard/auth/login.blade.php
+++ resources/views/dashboard/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Admin Stark | Log in</title>
+  <title>Admin Stark Brokers | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href=""><b>{{env('app_name')}}</b></a>
+    <a href=""><b>{{config('app.name')}}</b></a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
