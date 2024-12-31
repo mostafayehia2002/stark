ec407666ec2e1baddd8b0b8a1d731cd5cf9bb15c
mostafa yehia <96825436+mostafayehia2002@users.noreply.github.com>
2024-12-31 23:14:01 +0200

    build frontend

--- resources/js/services/axiosInstance.js
+++ resources/js/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Base URL configuration
-const API_URL = import.meta.env.APP_URL || 'https://stark.sa'
+const API_URL = import.meta.env.APP_URL || 'https://www.starkbrokers.com'
 
 const axiosInstance = axios.create({
   baseURL: `${API_URL}/api/v1`, // Add /api/v1 to base URL to match API endpoints
